85a99ad2a2a056d10e4490a0bee5bf64a5f43257
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-11 23:04:19 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,8 +7,6 @@ def minOperations(n):
     """ Given a number n, calculates the fewest number of operations
     needed to result in exactly n H characters in the file"""
     res = 0
-    if n is None:
-        return (0)
     for i in range(2, (n + 1)):
         while (n % i == 0):
             res = res + i
