cd746fe90400d27e0000958bf69210a0f5dc57fe
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-11 23:28:26 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,7 +10,7 @@ def minOperations(n):
         return (0)
     res = 0
     for i in range(2, (n + 1)):
-        if n % i == 0:
+        while (n % i == 0):
             res+= i
             n = n / i
     return (res)
