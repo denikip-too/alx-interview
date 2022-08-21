bb9326fc6a882ae799558edea4ca85e47ad2b0d7
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-08-21 23:38:42 +0000

    ...

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,8 +2,22 @@
 """reads stdin line by line and computes metrics"""
 import sys
 import requests
+import signal
 
+
+def signal_handler(sig, frame):
+    """for ctrl+c"""
+    signal.signal(signal.SIGINT, original_sigint)
+    sys.exit(1)
+    signal.signal(signal.SIGINT, signal_handler)
 
 def log_stats():
     """print these statistics from the beginning
-    After every 10 lines and/or a keyboard interruption (CTRL + C)""" 
+    After every 10 lines and/or a keyboard interruption (CTRL + C)"""
+    for line in sys.stdin:
+        print(line)
+        line.rstrip()
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, sigint_handler)
+    log_stats()
