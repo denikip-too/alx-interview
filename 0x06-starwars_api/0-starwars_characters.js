928a7b822c4fa97ebb6972417332df27b05afc6f
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-09-11 19:44:00 +0000

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+
+const request = require('request');
+const films = 'https://swapi-api.hbtn.io/api/films/';
+const movieCharacters = `${films}${process.argv[2]}/`;
+request(movieCharacters, function (error, response) {
+  if (!error && response.status === 200) {
+    return response.json();
+  }
+  for (const key in response) {
+    if (key === characters) {
+      console.log(`${characters[key]}`)
+    }
+  }
+})
