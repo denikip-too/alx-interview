602f452770f67744d3e28f2e410f9d1d089d29d8
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-10-04 15:55:13 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,10 +6,12 @@ def isWinner(x, nums):
     """ Return: name of the player that won the most rounds.
     If the winner cannot be determined, return None
     """
+    if x < 1 or not nums:
+        return None
     res = 0
     for i in range(x):
         res ^= nums[i % x]
-    if res >= 0:
+    if res == 0:
         return ("Maria")
     else:
         return ("Ben")
